3496175e2399fbac107e1d1651a3cb58aa235d6f
ShuichiITAKURA <coolervinore@gmail.com>
2015-05-17 02:48:25 +0900

    photon

--- Assets/Scenes/Battle/Scripts/Battle.cs
+++ Assets/Scenes/Battle/Scripts/Battle.cs
@@ -30,10 +30,24 @@ public class Battle : MonoBehaviour {
 
 	public Data data { get; set; }
 
+	public static Battle instance { get; private set; }
+
+	public BattleData battleData { get; private set; }
+
+	public Character character { get; private set; }
+
+	private GameObject _startButton;
+	private GameObject _waitingLabel;
+
 	void Awake() 
 	{
+		instance = this;
 		data = new Data();
 		data.mode = Mode.Standby;
+		Transform canvas = transform.FindChild("Canvas");
+		_waitingLabel = canvas.FindChild("WaitingLabel").gameObject;
+		_startButton = canvas.FindChild("StartButton").gameObject;
+		_startButton.SetActive(false);
 	}
 
 	void Start () 
@@ -43,13 +57,14 @@ public class Battle : MonoBehaviour {
 
 	void JoinLobby()
 	{
-		PhotonNetwork.ConnectUsingSettings("1.0");
+		PhotonNetwork.ConnectUsingSettings("2.0");
 		PhotonNetwork.sendRate = 60;
 		PhotonNetwork.sendRateOnSerialize = 60;
 	}
 
 	public void OnClickStartButton()
 	{
+		Play();
 	}
 
 	void OnJoinedLobby()
@@ -63,14 +78,47 @@ public class Battle : MonoBehaviour {
 		if (PhotonNetwork.isMasterClient)
 		{
 			data.playerType = PlayerType.Monster;
+			_startButton.SetActive(true);
 		}
 		else
 		{
 			data.playerType = PlayerType.Packman;
 		}
 
-		PhotonNetwork.Instantiate(data.GetResourceName(), new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0), Quaternion.identity, 0);
+		battleData = PhotonNetwork.Instantiate("BattleData", Vector3.zero, Quaternion.identity, 0).GetComponent<BattleData>();
+		GameObject g = PhotonNetwork.Instantiate(data.GetResourceName(), new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0), Quaternion.identity, 0);
+		character = g.GetComponent<Character>();
 	}
 
+	const float Speed = 0.25f;
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+	    {
+			character.transform.position += Speed * Vector3.right;
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			character.transform.position -= Speed * Vector3.right;
+		}
+		else if (Input.GetKey(KeyCode.UpArrow))
+		{
+			character.transform.position += Speed * Vector3.up;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			character.transform.position -= Speed * Vector3.up;
+		}
+	}
+
+	public void Play()
+	{
+		if (PhotonNetwork.isMasterClient)
+		{
+			battleData.SendState(BattleData.State.Play);
+		}
+		_startButton.SetActive(false);
+		_waitingLabel.SetActive(false);
+    }
 
 }
---
+++ Assets/Scenes/Battle/Scripts/BattleData.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BattleData : Photon.MonoBehaviour {
+
+	public enum State
+	{
+		Play,
+	}
+
+	private Battle battle;
+
+	void Awake()
+	{
+		photonView.observed = this;
+		battle = Battle.instance;
+	}
+
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting) {
+			//データの送信
+//			stream.SendNext(battle.data.mode);
+		} else {
+			//データの受信
+//			battle.data.mode = (Battle.Mode)stream.ReceiveNext();
+		}
+	}
+
+	public void SendState(State state)
+	{
+		photonView.RPC("ReceiveState", PhotonTargets.Others, (int)state);
+	}
+	
+	[RPC] 
+	void ReceiveState(int value)
+	{
+		print ("ReceiveState");
+		State state = (State)value;
+		switch (state)
+		{
+		case State.Play:
+			battle.Play();
+            break;
+        }
+    }
+}
