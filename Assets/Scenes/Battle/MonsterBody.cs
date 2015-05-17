6bffd625ee9311869eb1171137507dc3a1ce8893
ShuichiITAKURA <coolervinore@gmail.com>
2015-05-17 16:36:54 +0900

    キャラ向き モンスター画像

--- Assets/Scenes/Battle/MonsterBody.cs
+++ Assets/Scenes/Battle/MonsterBody.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class MonsterBody : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D c) {
-		print ("OnTriggerEnter2D");
 		if (c.tag == Packman.Tag)
 		{
 			Battle.instance.Hit(c.GetComponentInParent<Packman>());
--- Assets/Scenes/Battle/Scripts/CharacterAnimator.cs
+++ Assets/Scenes/Battle/Scripts/CharacterAnimator.cs
@@ -4,13 +4,68 @@ using System.Collections;
 public class CharacterAnimator : MonoBehaviour {
 	private Animator _animator;
 
+	public bool isWalkAnimation = true;
+	
+	protected Direction dir;
+	
+	private Vector3 _prevPosition;
+
+	public enum Direction
+	{
+		Wait,
+		Front,
+		Back,
+		Right,
+		Left,
+	}
+
 	void Awake()
 	{
 		_animator = GetComponent<Animator>();
+		_prevPosition = transform.position;
 	}
 
 	public void Play(string animationName)
 	{
 		_animator.Play(animationName);
 	}
+
+	void Start()
+	{
+		_prevPosition = transform.position;
+	}
+
+	void Update() {
+		if (_animator != null && isWalkAnimation)
+		{
+			print (transform.position);
+			Direction nextDir = dir;
+			if (transform.position.x > _prevPosition.x)
+			{
+				nextDir = Direction.Right;
+			}
+			else if (transform.position.x < _prevPosition.x)
+			{
+				nextDir = Direction.Left;
+			}
+			else if (transform.position.y > _prevPosition.y)
+			{
+				nextDir = Direction.Back;
+			}
+			else if (transform.position.y < _prevPosition.y)
+			{
+				nextDir = Direction.Front;
+			}
+			
+			print (nextDir);
+			
+			if (nextDir != dir && nextDir != Direction.Wait)
+			{
+				_animator.Play(nextDir.ToString());
+				nextDir = dir;
+			}
+		}
+		_prevPosition = transform.position;
+	}
+
 }
