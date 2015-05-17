da8619c2cd4c10815eb59a737fcf03175743f582
ShuichiITAKURA <coolervinore@gmail.com>
2015-05-17 17:57:22 +0900

    Battle Heaeer等

---
+++ Assets/HeaderIcon.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeaderIcon : MonoBehaviour {
+
+	[SerializeField] Sprite[] icons;
+	[SerializeField] SpriteRenderer _rendere;
+
+	public void Show(int id)
+	{
+		_rendere.sprite = icons[id];
+	}
+}
