8ada5a88c7516ce91758feb802706733461a4d7d
hikashiseiichiro <hikashi@cloverlab.jp>
2015-05-17 11:55:30 +0800

    コミット漏れ

---
+++ Assets/Scenes/Battle/Scripts/MonsterSensor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterSensor : MonoBehaviour {
+
+	GameObject BK;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if(c.gameObject.name == "SensorTriger"){
+			if(!BK){
+				BK = GameObject.Find("Bk");
+			}
+			BK.GetComponent<SpriteRenderer>().color = Color.red;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D c) {
+		if(c.gameObject.name == "SensorTriger"){
+			if(!BK){
+				BK = GameObject.Find("Bk");
+			}
+			BK.GetComponent<SpriteRenderer>().color = Color.white;
+		}
+	} 
+}
