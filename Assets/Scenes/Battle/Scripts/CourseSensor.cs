670d81b152c20e238f101cc7dde556dc1ac0f6c6
hikashiseiichiro <hikashi@cloverlab.jp>
2015-05-17 15:14:51 +0800

    ワープっぽいもの

--- Assets/Scenes/Battle/Scripts/CourseSensor.cs
+++ Assets/Scenes/Battle/Scripts/CourseSensor.cs
@@ -43,54 +43,103 @@ public class CourseSensor : MonoBehaviour {
 			if(myPosition == "body"){
 				float moveX = dammy.moveX;
 				float moveY = dammy.moveY;
-				if(moveX != 0){
-					if(moveX > 0){
-						if(!dammy.CanRight){
-							dammy.moveX = 0;
+				if(c.name.Substring(0,4) == "warp"){
+					GameObject rev_warp;
+					if(moveX != 0){
+						Vector2 parentPosition = dammy.transform.position;
+						Vector2 Scale = GetComponent<CircleCollider2D>().bounds.size;
+						Vector2 WallPosition;
+						Vector2 WallScale;
+						if(c.name.Substring(c.name.Length-1) == "l"){
+							if(moveX < 0){
+								rev_warp = GameObject.Find(c.name.Substring(0,c.name.Length-1)+"r");
+								WallPosition = rev_warp.transform.position;
+								WallScale = rev_warp.GetComponent<BoxCollider2D>().bounds.size;
+								parentPosition.x = WallPosition.x-(WallScale.x/2)-(Scale.x/2);
+								dammy.transform.position = parentPosition;
+							}
+						} else if(c.name.Substring(c.name.Length-1) == "r"){
+							if(moveX > 0){
+								rev_warp = GameObject.Find(c.name.Substring(0,c.name.Length-1)+"l");
+								WallPosition = rev_warp.transform.position;
+								WallScale = rev_warp.GetComponent<BoxCollider2D>().bounds.size;
+								parentPosition.x = WallPosition.x+(WallScale.x/2)+(Scale.x/2);
+								dammy.transform.position = parentPosition;
+							}
 						}
-					} else {
-						if(!dammy.CanLeft){
-							dammy.moveX = 0;
+					} else if(moveY != 0){
+						Vector2 parentPosition = dammy.transform.position;
+						Vector2 Scale = GetComponent<CircleCollider2D>().bounds.size;
+						Vector2 WallPosition;
+						Vector2 WallScale;
+						if(c.name.Substring(c.name.Length-1) == "u"){
+							if(moveY < 0){
+								rev_warp = GameObject.Find(c.name.Substring(0,c.name.Length-1)+"d");
+								WallPosition = rev_warp.transform.position;
+								WallScale = rev_warp.GetComponent<BoxCollider2D>().bounds.size;
+								parentPosition.y = WallPosition.y-(WallScale.y/2)-(Scale.y/2);
+								dammy.transform.position = parentPosition;
+							}
+						} else if(c.name.Substring(c.name.Length-1) == "d"){
+							if(moveY > 0){
+								rev_warp = GameObject.Find(c.name.Substring(0,c.name.Length-1)+"u");
+								WallPosition = rev_warp.transform.position;
+								WallScale = rev_warp.GetComponent<BoxCollider2D>().bounds.size;
+								parentPosition.x = WallPosition.y+(WallScale.y/2)+(Scale.y/2);
+								dammy.transform.position = parentPosition;
+							}
 						}
 					}
-				} else if(moveY != 0){
-					if(moveY > 0){
-						if(!dammy.CanUp){
-							dammy.moveY = 0;
-						}
-					} else {
-						if(!dammy.CanDown){
-							dammy.moveY = 0;
-						}
-					}
-				}
-				/*
-				if(c.gameObject.name != "UpSensor" && c.gameObject.name != "DownSensor" && c.gameObject.name != "LeftSensor" && c.gameObject.name != "RightSensor" ){
-					Debug.Log("body");
-					Vector2 parentPosition = parent.transform.position;
-					Vector2 Scale = GetComponent<CircleCollider2D>().bounds.size;
-					Vector2 WallPosition = c.transform.position;
-					Vector2 WallScale = c.bounds.size;
-					if(parent.GetComponent<Character>().moveX != 0){
-						if(parent.GetComponent<Character>().moveX > 0){
-							parentPosition.x = WallPosition.x-(WallScale.x/2)-(Scale.x/2)-(Scale.x/100);
+				} else {
+					if(moveX != 0){
+						if(moveX > 0){
+							if(!dammy.CanRight){
+								dammy.moveX = 0;
+							}
 						} else {
-							parentPosition.x = WallPosition.x+(WallScale.x/2)+(Scale.x/2)+(Scale.x/100);
+							if(!dammy.CanLeft){
+								dammy.moveX = 0;
+							}
 						}
-						parent.transform.position = parentPosition;
-						parent.GetComponent<Character>().moveX = 0;
-					}
-					if(parent.GetComponent<Character>().moveY != 0){
-						if(parent.GetComponent<Character>().moveY > 0){
-							parentPosition.y = WallPosition.y-(WallScale.y/2)-(Scale.y/2)-(Scale.y/100);
+					} else if(moveY != 0){
+						if(moveY > 0){
+							if(!dammy.CanUp){
+								dammy.moveY = 0;
+							}
 						} else {
-							parentPosition.y = WallPosition.y+(WallScale.y/2)+(Scale.y/2)+(Scale.y/100);
+							if(!dammy.CanDown){
+								dammy.moveY = 0;
+							}
 						}
-						parent.transform.position = parentPosition;
-						parent.GetComponent<Character>().moveY = 0;
 					}
+					/*
+					if(c.gameObject.name != "UpSensor" && c.gameObject.name != "DownSensor" && c.gameObject.name != "LeftSensor" && c.gameObject.name != "RightSensor" ){
+						Debug.Log("body");
+						Vector2 parentPosition = parent.transform.position;
+						Vector2 Scale = GetComponent<CircleCollider2D>().bounds.size;
+						Vector2 WallPosition = c.transform.position;
+						Vector2 WallScale = c.bounds.size;
+						if(parent.GetComponent<Character>().moveX != 0){
+							if(parent.GetComponent<Character>().moveX > 0){
+								parentPosition.x = WallPosition.x-(WallScale.x/2)-(Scale.x/2)-(Scale.x/100);
+							} else {
+								parentPosition.x = WallPosition.x+(WallScale.x/2)+(Scale.x/2)+(Scale.x/100);
+							}
+							parent.transform.position = parentPosition;
+							parent.GetComponent<Character>().moveX = 0;
+						}
+						if(parent.GetComponent<Character>().moveY != 0){
+							if(parent.GetComponent<Character>().moveY > 0){
+								parentPosition.y = WallPosition.y-(WallScale.y/2)-(Scale.y/2)-(Scale.y/100);
+							} else {
+								parentPosition.y = WallPosition.y+(WallScale.y/2)+(Scale.y/2)+(Scale.y/100);
+							}
+							parent.transform.position = parentPosition;
+							parent.GetComponent<Character>().moveY = 0;
+						}
+					}
+					*/
 				}
-				*/
 			} else if(c.gameObject.name != "Body"){
 				dammy.hitCourse(myPosition,false);
 			}
